85f34b5bc02e6eedbe216246e9ddad2b90412807
Richalien <Richalien@yandex.ru>
2020-09-13 04:23:08 +0400

    добавление роутера

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import YandexMap from "./component/YandexMap/YandexMap";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <YandexMap/>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Route exact path='/' render={() => <YandexMap/>}/>
+                {/*<Route exact path='/description' render={() => <AlertDescription/>}/>*/}
+            {/*  после хорошего сна допилить*/}
+            </div>
+        </BrowserRouter>
     );
 }
 
